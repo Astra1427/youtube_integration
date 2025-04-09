8b365a322bfcb061cdaf7996c432fc44ff07513e
Yousef Mohamed flutter <yousef.mohamed.flutter@appyinnovator.com>
2025-04-09 11:25:14 +0200

    Initialize Dart package with essential files and structure

---
+++ example/youtube_integration_example.dart
@@ -0,0 +1,6 @@
+import 'package:youtube_integration/youtube_integration.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/youtube_integration_test.dart
@@ -0,0 +1,16 @@
+import 'package:youtube_integration/youtube_integration.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
