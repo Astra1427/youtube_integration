0675ec07fc34c3d93ba3cd82782333fa5866f95a
Yousef Mohamed flutter <yousef.mohamed.flutter@appyinnovator.com>
2025-04-10 20:29:49 +0200

    refactor: Remove unused imports and simplify initial test structure

--- test/youtube_integration_test.dart
+++ test/youtube_integration_test.dart
@@ -1,16 +1,11 @@
-import 'package:youtube_integration/youtube_integration.dart';
 import 'package:test/test.dart';
 
 void main() {
   group('A group of tests', () {
-    final awesome = Awesome();
-
     setUp(() {
       // Additional setup goes here.
     });
 
-    test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
-    });
+    test('First Test', () {});
   });
 }
